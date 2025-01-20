32e36fd205e01f751b1459b66ce3c0d4515f8bab
Zaki-Essay <zakaressaiydy@gmail.com>
2025-01-20 11:55:23 +0100

    first commit

---
+++ src/main/java/me/gaga/springbootkafkabatchprocessing/SpringBootKafkaBatchProcessingApplication.java
@@ -0,0 +1,13 @@
+package me.gaga.springbootkafkabatchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaBatchProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaBatchProcessingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/gaga/springbootkafkabatchprocessing/SpringBootKafkaBatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gaga.springbootkafkabatchprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaBatchProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
